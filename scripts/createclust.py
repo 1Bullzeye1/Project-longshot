c4db7fa2e992d827b0978d81e74f6975f94fab4b
bullzeye <may.sonawane22@gmail.com>
2022-06-27 06:51:45 +0530

    update to project

--- scripts/createclust.py
+++ scripts/createclust.py
@@ -1,12 +1,19 @@
-import sys
-from google.cloud import storage
-from google.cloud import bigquery
 from google.cloud import dataproc_v1 as dataproc
 
+
 def create_cluster(project_id, region, cluster_name):
+    """This sample walks a user through creating a Cloud Dataproc cluster
+    using the Python client library.
+
+    Args:
+        project_id (string): Project to use for creating resources.
+        region (string): Region where the resources should live.
+        cluster_name (string): Name to use for creating a cluster.
+    """
+
     # Create a client with the endpoint set to the desired cluster region.
     cluster_client = dataproc.ClusterControllerClient(
-        client_options = {"api_endpoint": "{}-dataproc.googleapis.com:443".format(region)}
+        client_options={"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
     )
 
     # Create the cluster config.
@@ -21,24 +28,12 @@ def create_cluster(project_id, region, cluster_name):
 
     # Create the cluster.
     operation = cluster_client.create_cluster(
-        request = {"project_id": project_id, "region": region, "cluster": cluster}
+        request={"project_id": project_id, "region": region, "cluster": cluster}
     )
     result = operation.result()
 
     # Output a success message.
     print(f"Cluster created successfully: {result.cluster_name}")
-    # [END dataproc_create_cluster]
 
 
-if __name__ == "__main__":
-    # if len(sys.argv) < 4:
-    #     sys.exit("python create_cluster.py project_id region cluster_name")
-    #
-    # project_id = sys.argv[1]
-    # region = sys.argv[2]
-    # cluster_name = sys.argv[3]
-    # create_cluster(project_id, region, cluster_name)
-    project_id = "procrastinated-city-46778"
-    region = "US"
-    cluster_name = "Project_cluster"
-    create_cluster(project_id,region,cluster_name)
+create_cluster("procrastinated-city-46778","US","new_project")
